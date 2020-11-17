5ec9ec76fd853f609f8d474841f38b222fc5a61d
wyang19a <wyang19a@gmail.com>
2020-11-17 15:02:07 +0900

    Adds chat session model

---
+++ app/models/chatsession.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const messageSchema = require('./message')
+
+const chatSessionSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    messages: [messageSchema]
+  },
+  {
+    timestamps: true
+  }
+)
+
+module.exports = mongoose.model('ChatRoom', chatSessionSchema)
